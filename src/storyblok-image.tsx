064340b15f5a800abbeb4b550da08cc2c2866101
Felix Gnass <felix@lionizers.com>
2025-04-10 12:59:04 +0200

    feat: add asset image component

--- src/storyblok-image.tsx
+++ src/storyblok-image.tsx
@@ -2,7 +2,32 @@
 
 import Image, { ImageProps } from "next/image";
 import { storyblokImageLoader } from "./image-loader";
+import { Asset } from "./storyblok-types";
+import { getImageProps } from "./assets";
 
 export function StoryblokImage(props: ImageProps) {
   return <Image {...props} loader={storyblokImageLoader} />;
+}
+
+type AssetImageProps = {
+  asset?: Asset;
+  className?: string;
+  aspect?: number;
+  sizes?: string;
+};
+
+export function AssetImage({
+  asset,
+  aspect,
+  sizes,
+  ...props
+}: AssetImageProps) {
+  if (!asset?.filename) return null;
+  return (
+    <StoryblokImage
+      {...getImageProps(asset, { aspect })}
+      sizes={sizes}
+      {...props}
+    />
+  );
 }
